ccdf34c864db7a0453248c0f03e4721e14e48224
Sunder Newar <60382179+newarsunder@users.noreply.github.com>
2023-03-30 08:18:17 +0530

    Create StarSquare.py

---
+++ StarSquare.py
@@ -0,0 +1,27 @@
+n = 2
+len = 4*(n-1)+3
+x = int(len)
+a = 0
+b = n-1
+y = 0
+for i in range(1,x+1):
+    if i<len//2+1:
+        if i%2==0:
+            print("*   "*a + "        "*b + "*   "*a)
+            b -= 1
+        else:
+            print("*   " * a + "* " * x + "  * " * a)
+            a += 1
+            x -= 4
+            # print(0)
+
+    else:
+        if i%2==0:
+            print("*   " * a + "        " * b + "*   " * a)
+            b += 1
+        else:
+            a -= 1
+            x += 4
+            print("*   " * a + "* " * x + "  * " * a)
+
+
